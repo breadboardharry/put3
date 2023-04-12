62748d353230fc604f416558193c919beaa5b23e
HarryOnBreadboard <a.leroux330@laposte.net>
2023-04-12 20:28:55 +0200

    Volume loop and script name prompt in ps1_to_exe

--- script/run/ps1_to_exe.ps1
+++ script/run/ps1_to_exe.ps1
@@ -1,3 +1,4 @@
-﻿Invoke-ps2exe ".\run.ps1" ".\run.exe"
+﻿$filename = Read-Host "Script to convert: "
+Invoke-ps2exe ".\$filename.ps1" ".\$filename.exe"
 Write-Host "Opération terminée"
 Start-Sleep -Seconds 1
