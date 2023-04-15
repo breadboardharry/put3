5fab17cdcf8efcdcfc28327f897a059866d66264
HarryOnBreadboard <a.leroux330@laposte.net>
2023-04-15 15:30:12 +0200

    Socket module

--- server/index.js
+++ server/index.js
@@ -1,91 +1,54 @@
 const express = require('express');
 const app = express();
-
 const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Routes = require('./routes/routes');
-const utils = require('./utils/utils');
 
 // Constants and options
 const PORT = process.env.PORT || 3000;
+// Cors options - allow requests from any origin
 const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 };
 
-// Setup servers
-const server = http.createServer(app);
-const sockets = require('socket.io')(server, {
-  cors: corsOptions,
-  path: '/socket/',
-});
+/* -------------------------------------------------------------------------- */
+/*                                 HTTP SERVER                                */
+/* -------------------------------------------------------------------------- */
 
-// Allow requests from any origin
+/* ------------------------------ Configuration ----------------------------- */
+// Cors
 app.use(cors(corsOptions));
-
-// Configure body limits
+// Body limits
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-// Routes
-app.use('/', Routes);
+/* ----------------------------------- Setup --------------------------------- */
+// Create server
+const server = http.createServer(app);
 
-let users = {};
+// Setup routes
+app.use('/', Routes);
 
 // Server starting
 server.listen(PORT, () => {
   console.log(`[*] Server started on port ${PORT}`);
-
-  // setInterval(() => {
-  //   console.log('Users: ' + JSON.stringify(users));
-  // }, 2000);
 });
 
-// On each client connection
-sockets.on('connection', socket => {
-  console.log('[-] New user connected: ' + socket.id);
-  users[socket.id] =  {
-    status: 'pending',
-  };
-  socket.emit('id', socket.id);
+/* -------------------------------------------------------------------------- */
+/*                              WEB SOCKET SERVER                             */
+/* -------------------------------------------------------------------------- */
 
-  socket.on('disconnect', (data) => {
-    console.log('[-] User disconnected: ' + socket.id);
-    delete users[socket.id];
+// Server options
+const socketOptions = {
+  cors: corsOptions,
+  path: '/socket/',
+}
 
-    // Send updated fool list
-    const foolList = Object.values(utils.toArray(users)).filter(user => user.role == 'fool');
-    sockets.emit('foolList', foolList);
-  });
+// Setup server
+const io = require('socket.io')(server, socketOptions);
 
-  socket.on('role', (role) => {
-    console.log('[-] User ' + socket.id + ' selected role ' + role);
-    users[socket.id].status = role == 'fool' ? 'editing' : 'active';
-    users[socket.id].role = role;
-
-    if (role == 'fool') users[socket.id].name = utils.newName(users);
-
-    // Send updated fool list
-    const foolList = Object.values(utils.toArray(users)).filter(user => user.role == 'fool');
-    sockets.emit('foolList', foolList);
-  });
-
-  socket.on('status', (status) => {
-    console.log('[-] User ' + socket.id + ' new status ' + status);
-    users[socket.id].status = status;
-
-    // Send updated fool list
-    const foolList = Object.values(utils.toArray(users)).filter(user => user.role == 'fool');
-    sockets.emit('foolList', foolList);
-  });
-
-  socket.on('action', (data) => {
-    console.log('[-] Action from ' + socket.id + ' to ' + data.target.id);
-    sockets.emit('action', data);
-  });
-});
-
-
-
+// Setup events
+require('./socket')(io);
