d28099e9aa597d0d8e690c53f2c769e4d30c3e37
Harry On Breadboard <a.leroux330@laposte.net>
2025-09-28 19:31:14 +0200

    Fix desktop item audio and cleanup

--- apps/client/src/app/view/interaction/audio-player/audio-player.component.ts
+++ apps/client/src/app/view/interaction/audio-player/audio-player.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { InteractionTemplateComponent } from '../template/interaction-template.component';
-import { AudioService } from 'src/app/services/audio-service/audio.service';
 import { APIService } from 'src/app/services/api/api.service';
+import { AudioService } from 'src/app/services/audio-service/audio.service';
+import { InteractionTemplateComponent } from '../template/interaction-template.component';
 
 @Component({
     selector: 'app-audio-player',
     templateUrl: './audio-player.component.html',
-    styleUrls: ['./audio-player.component.scss'],
 })
 export class AudioPlayerComponent extends InteractionTemplateComponent {
     constructor(private audioService: AudioService, private api: APIService) {
@@ -15,7 +14,7 @@ export class AudioPlayerComponent extends InteractionTemplateComponent {
 
     init() {
         // Play sound when component is created
-        this.audioService.play(this.api.serverUrl + '/' + this.data.track.href);
+        this.audioService.play(this.api.serverUrl + '/' + this.data.track.src);
     }
 
     onHover(hover: boolean): void {}
